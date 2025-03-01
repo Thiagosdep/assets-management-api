091341128d779d576a7cbc2bdee14b239d8a6f77
Thiagosdep <thiago_sousap@hotmail.com>
2025-03-01 11:55:48 -0300

    :white_check_mark: feat(controller): add API versioning using URI strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppLogger } from './infrasctructure/logger/logger.service';
 
-@Controller()
+@Controller({ version: '1' })
 export class AppController {
   constructor(
     private readonly appService: AppService,
