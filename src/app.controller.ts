97a01d419b7e9df92f329651156065c14f505a07
Thiagosdep <thiago_sousap@hotmail.com>
2025-03-01 11:49:20 -0300

    :package: feat(infra): add log module with pino

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppLogger } from './infrasctructure/logger/logger.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly appLogger: AppLogger,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.appLogger.log('appLogger.log its ok!');
+    this.appLogger.error(
+      'appLogger.error its ok!',
+      new Error('Testing trace').stack,
+    );
+    this.appLogger.warn('appLogger.warn its ok!');
     return this.appService.getHello();
   }
 }
